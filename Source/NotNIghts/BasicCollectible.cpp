e0ed7fa4c5ccb07da94ece25517909d882159f31
Static Void <klinye@usc.edu>
2025-04-30 20:44:40 -0700

    basic collectible code

--- Source/NotNIghts/BasicCollectible.cpp
+++ Source/NotNIghts/BasicCollectible.cpp
@@ -3,12 +3,18 @@
 
 #include "BasicCollectible.h"
 
+#include "NotNIghtsCharacter.h"
+
 // Sets default values
 ABasicCollectible::ABasicCollectible()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("BoxCollider"));
+	SphereComponent->InitSphereRadius(100.0f);
+	RootComponent = SphereComponent;
 
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ABasicCollectible::OnOverlap);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +22,14 @@ void ABasicCollectible::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ABasicCollectible::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->IsA(ANotNIghtsCharacter::StaticClass()))
+	{
+		OnDirectCollect();
+	}
 }
 
 // Called every frame
--- Source/NotNIghts/BasicCollectible.h
+++ Source/NotNIghts/BasicCollectible.h
@@ -14,11 +14,11 @@ class NOTNIGHTS_API ABasicCollectible : public AActor, public ICollectible
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	UStaticMesh* RingMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ring Mesh", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* StaticMesh;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<USphereComponent> BoxComponent;
+	TObjectPtr<USphereComponent> SphereComponent;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -27,6 +27,9 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
 	// Called every frame
