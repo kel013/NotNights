6516f4b8ce90b9ba71d61ee19f6ac2b386af4f36
Static Void <klinye@usc.edu>
2026-03-23 17:02:19 -0700

    fixed halfrings

--- Source/NotNIghts/HalfRing.cpp
+++ Source/NotNIghts/HalfRing.cpp
@@ -10,7 +10,7 @@ void AHalfRing::SetUpRingObjects()
 		BoxComponent->SetBoxExtent(FVector(BoxThickness, Radius, (StraightLength+Radius)/2));
 		BoxComponent->SetRelativeLocation(FVector(0, 0, (StraightLength + Radius) / 2));
 	}
-	float CircleCircumference = 2 * Radius * PI;
+	float CircleCircumference = Radius * PI;
 	float RingLength = 2 * StraightLength + CircleCircumference;
 	int CircleNumberOfSections = (CircleCircumference / RingLength) * NumberOfSections;
 	if ((NumberOfSections % 2 == 0 && CircleNumberOfSections % 2 == 1) || (NumberOfSections % 2 == 1 && CircleNumberOfSections % 2 == 0))
@@ -23,7 +23,7 @@ void AHalfRing::SetUpRingObjects()
 	for (int x = 0; x < LengthWiseNumberOfSections / 2; x++)
 	{
 		FString MeshStringLeft = "RingBall";
-		MeshStringLeft.Append(FString::FromInt(2*x));
+		MeshStringLeft.Append(FString::FromInt(2 * x));
 		FName LeftMeshName = FName(*MeshStringLeft);
 		UStaticMeshComponent* LeftRingStaticMeshComponent;
 
@@ -37,11 +37,12 @@ void AHalfRing::SetUpRingObjects()
 		LeftRingStaticMeshComponent->SetRelativeScale3D(FVector(MeshScale, MeshScale, MeshScale));
 		FVector LocationLeft = FVector::LeftVector * Radius;
 		LocationLeft += FVector::UpVector * x * SpaceBetweenSections;
+		LocationLeft -= FVector(0, 0, (StraightLength + Radius) / 2);
 		LeftRingStaticMeshComponent->SetRelativeLocation(LocationLeft);
 		RingStaticMesh.Emplace(LeftRingStaticMeshComponent);
 
 		FString MeshStringRight = "RingBall";
-		MeshStringRight.Append(FString::FromInt(2 * x));
+		MeshStringRight.Append(FString::FromInt(2 * x + 1));
 		FName RightMeshName = FName(*MeshStringRight);
 		UStaticMeshComponent* RightRingStaticMeshComponent;
 
@@ -55,16 +56,17 @@ void AHalfRing::SetUpRingObjects()
 		RightRingStaticMeshComponent->SetRelativeScale3D(FVector(MeshScale, MeshScale, MeshScale));
 		FVector LocationRight = FVector::RightVector * Radius;
 		LocationRight += FVector::UpVector * x * SpaceBetweenSections;
+		LocationRight -= FVector(0, 0, (StraightLength + Radius) / 2);
 		RightRingStaticMeshComponent->SetRelativeLocation(LocationRight);
 		RingStaticMesh.Emplace(RightRingStaticMeshComponent);
 	}
 
-	double Degree = 180.0f / CircleNumberOfSections;
-	FVector HalfCircleCenter = FVector::UpVector * StraightLength;
+	double Degree = 180.0f / (CircleNumberOfSections-1);
+	FVector HalfCircleCenter = FVector::UpVector * (StraightLength - ((StraightLength + Radius) / 2));
 	for (int x = 0; x < CircleNumberOfSections; x++)
 	{
 		FString MeshString = "RingBall";
-		MeshString.Append(FString::FromInt(LengthWiseNumberOfSections / 2 + x));
+		MeshString.Append(FString::FromInt(LengthWiseNumberOfSections + x));
 		FName MeshName = FName(*MeshString);
 		UStaticMeshComponent* RingStaticMeshComponent;
 
@@ -77,9 +79,11 @@ void AHalfRing::SetUpRingObjects()
 
 		RingStaticMeshComponent->SetRelativeScale3D(FVector(MeshScale, MeshScale, MeshScale));
 		FVector Location = FVector::UpVector * Radius;
-		Location = Location.RotateAngleAxis(Degree * x, FVector::ForwardVector);
+		Location = Location.RotateAngleAxis((Degree * x)-90, FVector::ForwardVector);
 		Location += HalfCircleCenter;
 		RingStaticMeshComponent->SetRelativeLocation(Location);
 		RingStaticMesh.Emplace(RingStaticMeshComponent);
 	}
+
+
 }
