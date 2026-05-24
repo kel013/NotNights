691ddc63c5f0e4607fa83b561c7e35bb937d1234
Static Void <klinye@usc.edu>
2026-05-23 18:45:45 -0700

    pause menu

--- Source/NotNIghts/NotNIghtsGameMode.cpp
+++ Source/NotNIghts/NotNIghtsGameMode.cpp
@@ -9,7 +9,7 @@
 ANotNIghtsGameMode::ANotNIghtsGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/EssentialUtility/Player.Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
